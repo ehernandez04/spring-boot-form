f1058a7817715e1bf7093d91c56ec2638f61419c
Erick Hernández <erickhernandez0492@gmail.com>
2023-08-11 22:53:13 +0200

    Creando la clase model del formulario

--- src/main/java/com/example/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/example/springboot/form/app/controllers/FormController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.form.app.controllers;
 
+import com.example.springboot.form.app.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,20 @@ public class FormController {
                            @RequestParam(name = "username") String username,
                            @RequestParam String password,
                            @RequestParam String email){
+
+        Usuario usuario = new Usuario();
+        usuario.setUsername(username);
+        usuario.setPassword(password);
+        usuario.setEmail(email);
+
+        /*
         model.addAttribute("titulo", "Resultado form");
         model.addAttribute("username", username);
         model.addAttribute("password", password);
         model.addAttribute("email", email);
-
+        */
+        model.addAttribute("titulo", "Resultado form");
+        model.addAttribute("usuario", usuario);
         return "resultado";
     }
 }
