385cf16fca153e274ad061a097da9d2338b4611f
Erick Hernández <erickhernandez0492@gmail.com>
2023-08-11 23:53:32 +0200

    Validación del formulario usando anotaciones

--- src/main/java/com/example/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/example/springboot/form/app/controllers/FormController.java
@@ -1,11 +1,16 @@
 package com.example.springboot.form.app.controllers;
 
 import com.example.springboot.form.app.models.domain.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -17,7 +22,7 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Usuario usuario, Model model/*,
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model/*,
                            @RequestParam(name = "username") String username,
                            @RequestParam String password,
                            @RequestParam String email*/){
@@ -33,7 +38,19 @@ public class FormController {
         model.addAttribute("password", password);
         model.addAttribute("email", email);
         */
+
         model.addAttribute("titulo", "Resultado form");
+
+        if(result.hasErrors()){
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err ->{
+                errores.put(err.getField(),"El campo " .concat(err.getField()).concat(" ").concat(err.getDefaultMessage()) );
+            });
+
+            model.addAttribute("error", errores);
+            return "form";
+        }
+
         model.addAttribute("usuario", usuario);
         return "resultado";
     }
--- src/main/java/com/example/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/example/springboot/form/app/models/domain/Usuario.java
@@ -1,9 +1,16 @@
 package com.example.springboot.form.app.models.domain;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Usuario {
 
+    @NotEmpty
     private String username;
+
+    @NotEmpty
     private String password;
+
+    @NotEmpty
     private String email;
 
     public String getUsername() {
