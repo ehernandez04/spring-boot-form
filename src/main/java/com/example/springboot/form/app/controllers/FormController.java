c18c81aa93862e5ce04aa68b9346296cf2bcb39f
Erick Hernández <erickhernandez0492@gmail.com>
2023-08-13 12:53:59 +0200

    @SessionAtributes

--- src/main/java/com/example/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/example/springboot/form/app/controllers/FormController.java
@@ -7,16 +7,23 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model){
         Usuario usuario = new Usuario();
+        usuario.setNombre("John");
+        usuario.setApellido("Doe");
+        usuario.setIdentificador("123.456.789-K");
 
         model.addAttribute("titulo", "Formulario usuarios");
         model.addAttribute("usuario", usuario);
@@ -24,7 +31,7 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario,  BindingResult result, Model model/*,
+    public String procesar(@Valid Usuario usuario,  BindingResult result, Model model, SessionStatus status/*,
                            @RequestParam(name = "username") String username,
                            @RequestParam String password,
                            @RequestParam String email*/){
@@ -55,6 +62,7 @@ public class FormController {
         }
 
         model.addAttribute("usuario", usuario);
+        status.setComplete();
         return "resultado";
     }
 }
